00ba947aacb42c22f2d0279e81f93779c15c5771
wizchordz <43661569+wizchordz@users.noreply.github.com>
2026-07-19 00:06:00 +0000

    Fix Metro bundle: add reanimated/worklets babel plugin, disable beta reactCompiler, add stacktrace to Gradle

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -2,5 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
+    plugins: [
+      // Required for react-native-reanimated 4.x + react-native-worklets.
+      // This plugin transforms `'worklet'` directives so worklet functions
+      // can run on the UI thread. Must be listed last among plugins.
+      'react-native-reanimated/plugin',
+    ],
   };
 };
